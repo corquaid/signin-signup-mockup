009509b5eb2674c450361c5f90ca9ecd33e32930
Cormac Quaid <thequad@gmail.com>
2021-10-01 15:36:00 +0200

    Update package.json

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styles from "./styles/App.module.css";
 
 function App() {
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <div className={styles.appContainer}>
                 <div className={styles.contentContainer}>
                     <Switch>
